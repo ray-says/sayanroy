89864be9fabf4395cebaa4d2e9cbb126b1d7c557
SAYAN ROY <sr.roysayan@gmail.com>
2024-04-24 01:40:03 -0600

    Mouse Cursor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
+import AnimatedCursor from "react-animated-cursor"
+import { isMobile } from 'react-device-detect'
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,7 +10,26 @@ import Project from './components/Project';
 
 
 function App() {
+  const isMobileDevice = isMobile;
   return (
+    <>
+    {!isMobileDevice && (
+      <AnimatedCursor
+        color='0, 161, 155'
+        innerSize={5}
+        outerSize={25}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+        backgroundColor: '#00a19b'
+        }}
+        outerStyle={{
+          border: '1px solid #00a19b',
+        }}
+      />
+    )}
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element ={<Home />} />
@@ -18,6 +39,7 @@ function App() {
       </Route>
 
     </Routes>
+    </>
   )
 }
 
